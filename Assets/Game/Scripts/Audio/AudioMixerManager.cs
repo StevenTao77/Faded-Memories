293d02fa419c68f15adabd475711b5b5ed21abd1
VilJauh <157316437+VilJauh@users.noreply.github.com>
2026-03-26 14:57:09 +0200

    Added few audio ambient zones and made new audiomixer group

--- Assets/Game/Scripts/Audio/AudioMixerManager.cs
+++ Assets/Game/Scripts/Audio/AudioMixerManager.cs
@@ -21,4 +21,8 @@ public class AudioMixerManager : MonoBehaviour
     {
         audioMixer.SetFloat("ambienceVolume", Mathf.Log10(level) * 20f);
     }
+    public void SetMusicVolume(float level)
+    {
+        audioMixer.SetFloat("musicVolume", Mathf.Log10(level) * 20f);
+    }
 }
