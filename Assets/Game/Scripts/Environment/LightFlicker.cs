c32fee3f795ae687f0ca3069ecb740dfecc7da60
Kappe6000 <kappe.salonen@gmail.com>
2026-04-24 17:43:03 +0300

    Torch light and sfx + darker outside + lantern light
    LightFlicker script can be used to make any wanted light source to flicker.

---
+++ Assets/Game/Scripts/Environment/LightFlicker.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    private Light lightToFlicker;
+    [SerializeField, Range(0f, 10f)] private float minIntensity = 0.5f;
+    [SerializeField, Range(0f, 10f)] private float maxIntensity = 5f;
+    [SerializeField, Min(0f)] private float timebetweenIntensity = 0.1f;
+
+    private float currentTimer;
+
+    private void Awake()
+    {
+        if (lightToFlicker == null)
+        {
+            lightToFlicker = GetComponent<Light>();
+        }
+        ValidateIntensityBounds();
+    }
+
+    private void Update()
+    {
+        currentTimer += Time.deltaTime;
+        if (!(currentTimer >= timebetweenIntensity)) return;
+        lightToFlicker.intensity = Random.Range(minIntensity, maxIntensity);
+        currentTimer = 0f;
+    }
+
+    private void ValidateIntensityBounds()
+    {
+        if (!(minIntensity > maxIntensity)) return;
+        Debug.LogWarning("Min intensity is greater than max intensity, swapping values.");
+        (minIntensity, maxIntensity) = (maxIntensity, minIntensity);
+    }
+}
