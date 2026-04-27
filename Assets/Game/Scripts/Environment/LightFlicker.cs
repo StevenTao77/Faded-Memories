4e421056f1534e9565c93c595028cf00e55d0ce6
Steven77 <taoshiwen4@gmail.com>
2026-04-27 17:32:25 +0300

    Revert "Pulsating lights for interactable objects and cabin inside slightly darker"
    This reverts commit cb78d42148e5ee12b3dd85a92cd39d01dcabf7cd.

--- Assets/Game/Scripts/Environment/LightFlicker.cs
+++ Assets/Game/Scripts/Environment/LightFlicker.cs
@@ -6,8 +6,6 @@ public class LightFlicker : MonoBehaviour
     [SerializeField, Range(0f, 200f)] private float minIntensity = 0.5f;
     [SerializeField, Range(0f, 200f)] private float maxIntensity = 5f;
     [SerializeField, Min(0f)] private float timebetweenIntensity = 0.1f;
-    public bool flicker = true;
-    public bool pulsate = false;
 
     private float currentTimer;
 
@@ -24,15 +22,8 @@ public class LightFlicker : MonoBehaviour
     {
         currentTimer += Time.deltaTime;
         if (!(currentTimer >= timebetweenIntensity)) return;
-        if (flicker)
-        {
-            lightToFlicker.intensity = Random.Range(minIntensity, maxIntensity);
-            currentTimer = 0f;
-        }
-        else if (pulsate && !flicker)
-        {
-            lightToFlicker.intensity = Mathf.Lerp(0, maxIntensity, Mathf.PingPong(Time.time, 1f));
-        }
+        lightToFlicker.intensity = Random.Range(minIntensity, maxIntensity);
+        currentTimer = 0f;
     }
 
     private void ValidateIntensityBounds()
